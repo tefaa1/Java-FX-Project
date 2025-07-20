e98f54bb5b0bc4d839c1a2e780bedc18b65a3d8e
tefaa1 <mh8369629@gmail.com>
2025-07-20 16:16:56 +0300

    Implementation main and test project Closes #2

--- src/main/java/org/example/demo1/HelloApplication.java
+++ src/main/java/org/example/demo1/HelloApplication.java
@@ -1,7 +1,23 @@
 package org.example.demo1;
 
-public class HelloApplication{
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class HelloApplication extends Application {
 
     public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Register.fxml"));
+        Scene scene = new Scene(root,600,500);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
     }
 }
